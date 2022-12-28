017a962190766d15fcf5a304596f864bcc9b7a41
Ronald <rvpostha15@yahoo.com>
2022-12-28 15:00:06 -0500

    lab solution

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(string) { //a function for each string in tutorials array
+    return string.split(' ').map(function(word) { //splitting each string up by each word (creating an array of words for each string)
+      return (word.charAt(0).toUpperCase() + word.slice(1)); //for each word we are capitalizing the first letter (.charAt(0)) and attaching (.slice) all the characters with the second letter(slice(1)) 
+    }).join(' '); // we are joining our new capitalized words together => our word array is turning back into a string
+  })
 }
+titleCased(tutorials) // we have to call our function so it actually does something
